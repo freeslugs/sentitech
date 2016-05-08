1e3270801e1b4fd9d4f35ed7ea11690587d3f45b
freeslugs <freeslugs@gmail.com>
2016-05-08 06:49:24 -0400

    modularize functions

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 import pos from "pos" 
 
 var express = require('express');
+var cors = require('cors');
 var app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+app.use(cors());
+
 app.get('/', function(req, res) {
   res.send('hello world');
 });
 
-var words = new pos.Lexer().lex('You have an ugly car.');
-var tagger = new pos.Tagger();
-var taggedWords = tagger.tag(words);
-for (var i in taggedWords) {
-  var taggedWord = taggedWords[i];
-  var word = taggedWord[0];
-  var tag = taggedWord[1];
-  console.log(word + " /" + tag);
-}
+app.listen(app.get('port') || 3000, function() {
+  console.log('Node app is running on port', app.get('port') || 3000);
+});
+
+// var words = new pos.Lexer().lex('You have an ugly car.');
+// var tagger = new pos.Tagger();
+// var taggedWords = tagger.tag(words);
+// for (var i in taggedWords) {
+//   var taggedWord = taggedWords[i];
+//   var word = taggedWord[0];
+//   var tag = taggedWord[1];
+//   console.log(word + " /" + tag);
+// }
 
 
 // https://tone-analyzer-demo.mybluemix.net/
--- appcompose/home/home.js
+++ appcompose/home/home.js
@@ -9,17 +9,66 @@
       jQuery('#set-subject').click(setSubject);
       jQuery('#get-subject').click(getSubject);
       jQuery('#add-to-recipients').click(addToRecipients);
+
+      getSentiment();
     });
   };
 
+  function getSentiment() {
+    getBody(function(result) {
+      // jQuery.get("localhost:3000").then(function(data) {
+      //   console.log(data);
+      // });
+      jQuery.ajax({
+          url: "https://localhost:3000",
+       
+          // The name of the callback parameter, as specified by the YQL service
+          jsonp: "callback",
+       
+          // Tell jQuery we're expecting JSONP
+          dataType: "jsonp",
+       
+          // Tell YQL what we want and that we want JSON
+          data: {
+              q: "select title,abstract,url from search.news where query=\"cat\"",
+              format: "json"
+          },
+       
+          // Work with the response
+          success: function( response ) {
+              console.log( response ); // server response
+          }
+      });
+    });
+  }
+
   function setSubject(){
-    Office.cast.item.toItemCompose(Office.context.mailbox.item).subject.setAsync('Hello world!');
+    // Office.cast.item.toItemCompose(Office.context.mailbox.item).subject.setAsync('Hello world!');
+    Office.context.mailbox.item.body.setAsync(
+      '<a id="LPNoLP" href="http://www.contoso.com">Click here!</a>', 
+      {coercionType: Office.CoercionType.Html});
   }
 
   function getSubject(){
-    Office.cast.item.toItemCompose(Office.context.mailbox.item).subject.getAsync(function(result){
-      app.showNotification('The current subject is', result.value);
-    });
+    // Office.cast.item.toItemCompose(Office.context.mailbox.item).subject.getAsync(function(result){
+    //   app.showNotification('The current subject is', result.value);
+    // });
+    
+  }
+
+  function setBody(text) {
+    Office.context.mailbox.item.body.setAsync(
+      text, 
+      {coercionType: Office.CoercionType.Html}
+    );
+  }
+
+  function getBody(callback) {
+    Office.context.mailbox.item.body.getAsync(
+      "text",
+      { asyncContext:"This is passed to the callback" },
+      callback
+    );
   }
 
   function addToRecipients(){
